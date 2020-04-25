164eedf488df6d7b46954c4a7f2675a711f66c72
JinHo-Ryzen <JinHo-Ryzen@DESKTOP-6HL78U3>
2020-04-26 02:15:03 +0900

    .

--- CodeTest/src/com/codetest/main/Main.java
+++ CodeTest/src/com/codetest/main/Main.java
@@ -1,8 +1,14 @@
 package com.codetest.main;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import com.codetest.model.vo.AESEncrypt;
+import com.codetest.model.vo.Board;
+import com.codetest.model.vo.ClassReflection;
 
 
 
@@ -10,47 +16,68 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-//		Board board = new Board();
-//		ClassReflection.selectList(board);
-//		System.out.println(board.toString());
+
 		
-//		File file = new File("./");
-//		System.out.println(file.getAbsolutePath());
-//		
-//		
-//		
-//		AESEncrypt aes = new AESEncrypt("./");
-//		
-//		String encryptName = aes.encrypt("둘리");
-//		System.out.println(encryptName);
-//		String name = aes.decrypt(encryptName);
-//		System.out.println(name);
+		
+	}
+	
+	
+	public static void websocket() {
+		try {
+			ServerSocket server  = new ServerSocket(80);
+			System.out.println("Server is start.. and client waiting..");
+			Socket client = server.accept();
+			System.out.println("client is connected..");
+			InputStream in = client.getInputStream();
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	
+	
+	
+	
+	public static void regex() {
 //		String newStr = str.replaceAll("(?==[^?])=", "바꿀 문자열");
+		Pattern pattern = Pattern.compile("(?==[^?])=");
+		String str = "SELECT * FROM (SELECT * FROM (SELECT A.*, ROWNUM AS R FROM (SELECT * FROM TRIP_TB WHERE CATEGORY=? AND TRAVLE_LOCALE=? AND TAG LIKE ?)A) WHERE R BETWEEN ? AND ?)B JOIN (SELECT TRIP_NO, COUNT(*)AS COUNT FROM (SELECT * FROM TRIP_TB  JOIN LIKE_TB ON TRIP_TB.NO=LIKE_TB.TRIP_NO) GROUP BY TRIP_NO)C ON B.NO=C.TRIP_NO ORDER BY WRITE_DATE DESC";
+		String replceStr = str.replaceAll("(?==[^?])=", "바꿀 문자열");
+		Matcher matcher = pattern.matcher(str);
+		int count = 0;
+		while (matcher.find()) {
+			count++;
+		}
+		matcher.reset();
+		int[] indexs = new int[count];
+		int i = 0;
+		while (matcher.find()) {
+			indexs[i] =  matcher.start();
+			System.out.println(indexs[i]);
+			i++;
+		}
+		System.out.println("indexs.length : " + indexs.length);
+		int targetIndex = indexs[0];
+		String newStr = str.substring(0, targetIndex) + "바꿀 문자열" + str.substring(targetIndex+1, str.length());
+		System.out.println(newStr);
 		
-	
-	Pattern pattern = Pattern.compile("(?==[^?])=");
-	String str = "SELECT * FROM (SELECT * FROM (SELECT A.*, ROWNUM AS R FROM (SELECT * FROM TRIP_TB WHERE CATEGORY=? AND TRAVLE_LOCALE=? AND TAG LIKE ?)A) WHERE R BETWEEN ? AND ?)B JOIN (SELECT TRIP_NO, COUNT(*)AS COUNT FROM (SELECT * FROM TRIP_TB  JOIN LIKE_TB ON TRIP_TB.NO=LIKE_TB.TRIP_NO) GROUP BY TRIP_NO)C ON B.NO=C.TRIP_NO ORDER BY WRITE_DATE DESC";
-	String replceStr = str.replaceAll("(?==[^?])=", "바꿀 문자열");
-	Matcher matcher = pattern.matcher(str);
-	int count = 0;
-	while (matcher.find()) {
-		count++;
 	}
-	matcher.reset();
-	int[] indexs = new int[count];
-	int i = 0;
-	while (matcher.find()) {
-		indexs[i] =  matcher.start();
-		System.out.println(indexs[i]);
-		i++;
+	
+	public static void AESEncrypt() {
+		AESEncrypt aes = new AESEncrypt("./");
+		
+		String encryptName = aes.encrypt("둘리");
+		System.out.println(encryptName);
+		String name = aes.decrypt(encryptName);
+		System.out.println(name);
 	}
-	System.out.println("indexs.length : " + indexs.length);
-	int targetIndex = indexs[0];
-	String newStr = str.substring(0, targetIndex) + "바꿀 문자열" + str.substring(targetIndex+1, str.length());
-	System.out.println(newStr);
 	
-	
-	
+	public static void classReflection() {
+		Board board = new Board();
+		ClassReflection.selectList(board);
+		System.out.println(board.toString());
 	}
 
 }
